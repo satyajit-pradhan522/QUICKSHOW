ed8e66b526e82249e57726a981ff699a434baaf7
satyajit-pradhan522 <satyajitpradhan522@gmail.com>
2025-11-24 23:56:06 +0530

    set tailwind & dom router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import Favorite from "./pages/Favorite";
+import MyBookings from "./pages/MyBookings";
+import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  return <div></div>;
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
 };
 
 export default App;
